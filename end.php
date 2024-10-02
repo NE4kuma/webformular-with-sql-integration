c0bc6cdd0b3bca354d5b1952043da857527f5e48
NE4 <130961032+NE4kuma@users.noreply.github.com>
2024-10-02 19:30:12 +0100

    Update end.php

--- end.php
+++ end.php
@@ -1,3 +1,24 @@
-<p
->
+<?php
+$hostname = "localhost";
+$username = "review_site";
+$password = "YJy22f-44";
+$db = "opinions";
 
+$dbconnect=mysqli_connect($hostname,$username,$password,$db);
+
+if ($dbconnect->connect_error) {
+    die("Database connection failed: " . $dbconnect->connect_error);
+}
+
+if(isset($_POST['submit'])) {
+    $reviewer_name=$_POST['reviewer_name'];
+    $details=$_POST['details'];
+    $query = "INSERT INTO user_review (reviewer_name, details)
+    VALUES ('$reviewer_name', '$details')";
+        if (!mysqli_query($dbconnect, $query)) {
+                die('An error occurred. Your review/opinion has not been submitted.');
+        } else {
+            echo "Thanks for your review/opinion.";
+        }
+}
+?>
